654492af65b60f4c0da2de281fa52af04dfc6ec6
toooweee <byprob03@gmail.com>
2025-07-06 23:54:40 +0500

    feat: (react) 29:30

---
+++ react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
+
+const App = () => {
+  return (
+    <section>
+      <Header />
+      <MainContent />
+      <Footer />
+    </section>
+  );
+};
+
+export default App;
---
+++ react/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Welcome to My Website!</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
